2ae82090a572c7a0ede9a04a994cb545546aca83
lesther-santana <lesthersan@gmail.com>
2021-09-19 23:03:24 -0400

    first commit

---
+++ trips/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Create your views here.
+def index(request):
+    pass
+
+def trips(request):
+    pass
+
+def profie(request):
+    pass
+
+def new_trip(request):
+    pass
+
+def log_in(request):
+    pass
+
+def create_user(request):
+    pass
