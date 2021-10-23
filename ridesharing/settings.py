141a674087266d5a6c267bd73815563f5e93ea1a
lesther-santana <lesthersan@gmail.com>
2021-10-23 17:48:09 -0400

    another fix

--- ridesharing/settings.py
+++ ridesharing/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['boleame.herokuapp.com']
+ALLOWED_HOSTS = ['boleame.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
